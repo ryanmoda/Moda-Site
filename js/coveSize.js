57589cccf181339dc6c6f89ed99ef5c063c8568b
Jack Phelan <jack@modalight.com>
2019-01-31 08:50:25 -0800

    variables for fixture height
    fix graze ext ies files

---
+++ js/coveSize.js
@@ -0,0 +1,3 @@
+document.getElementById("logo").src="svgs/modalogo.svg";
+document.getElementById("coveSize").innerHTML = "<p class='p1'>60mm H</p><p>2 3/10 inches</p>";
+
--- js/logo.js
+++ js/logo.js
@@ -1,3 +1,2 @@
 document.getElementById("logo").src="svgs/modalogo.svg";
-document.getElementById("mmcSize").innerHTML = "<p class='p1'>30mm Tall</p><p>1 3/16 inches</p>";
 
