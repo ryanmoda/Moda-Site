b3ccb933dac6930272d6a9333a5db21cfe2496b4
Jack Phelan <jack@modalight.com>
2019-02-20 13:03:16 -0800

    new home page close to done

--- js/logo.js
+++ js/logo.js
@@ -3,3 +3,11 @@ var logos = document.getElementsByClassName("logo");
 for (var i = 0; i < logos.length; i++) {
 	logos[i].src="svgs/modalogo.svg";
 }
+document.getElementById("phone").src="images/Icons/icon-phone-blk-sm.svg";
+document.getElementById("fax").src="images/Icons/icon-fax-blk.svg";
+document.getElementById("mail").src="images/Icons/icon-email-blk-sm.svg";
+
+document.getElementById("fb").src="svgs/FB-blk.svg";
+document.getElementById("ig").src="svgs/ig-black.svg";
+document.getElementById("tw").src="svgs/twitter-blk.svg";
+document.getElementById("in").src="svgs/linedkin-blk.svg";
--- js/logoWhite.js
+++ js/logoWhite.js
@@ -3,3 +3,11 @@ for (var i = 0; i < logos.length; i++) {
 	logos[i].src="svgs/modalogoWHITE.svg";
 }
 //document.getElementById("logo").src="svgs/modalogoWHITE.svg";
+document.getElementById("phone").src="images/Icons/icon-phone-wh.svg";
+document.getElementById("fax").src="images/Icons/icon-fax-wh.svg";
+document.getElementById("mail").src="images/Icons/icon-email-wh.svg";
+
+document.getElementById("fb").src="images/Icons/FB.svg";
+document.getElementById("ig").src="images/Icons/ig.svg";
+document.getElementById("tw").src="images/Icons/twitter.svg";
+document.getElementById("in").src="images/Icons/linedkin.svg";
