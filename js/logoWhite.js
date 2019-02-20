3d8228c9fd258c49cc675a425c3cc0f71301a6dd
Jack Phelan <jack@modalight.com>
2019-02-20 10:06:39 -0800

    working on home page

--- js/logoWhite.js
+++ js/logoWhite.js
@@ -1,1 +1,5 @@
-document.getElementById("logo").src="svgs/modalogoWHITE.svg";
+var logos = document.getElementsByClassName("logo");
+for (var i = 0; i < logos.length; i++) {
+	logos[i].src="svgs/modalogoWHITE.svg";
+}
+//document.getElementById("logo").src="svgs/modalogoWHITE.svg";
