116756dc257cd74a0f0fe423507cc9f0152485b0
Jack Phelan <jack@modalight.com>
2019-03-13 09:42:54 -0700

    change cove.html

--- js/script.js
+++ js/script.js
@@ -15,7 +15,7 @@
                 templateUrl:'mini-cove.html'
             })
 			.when('/cove',{
-                templateUrl:'cove.html'
+                templateUrl:'moda-cove.html'
             })
 					.when('/cove-wl',{
 						templateUrl:'cove-wl.html',
