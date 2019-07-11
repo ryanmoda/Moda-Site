efeaba2abadaa45745bac2df9f278687bd77cf4e
Jack Phelan <jack@modalight.com>
2019-07-11 11:18:17 -0700

    sitemap

---
+++ js/batch.py
@@ -0,0 +1,21 @@
+import re
+
+moda = "http://modalight.com/#/"
+
+regex = re.compile("(?<=\'\/)(.*?)(?=\')")
+with open("script2.js", 'r') as f:
+        f = f.read()
+        result = re.findall(regex, f)
+
+n = open("sitemap.xml", "w")
+n.write('<urlset xmlns="http://www.sitemaps.org/schemas/sitemap/0.9" xmlns:xsi="http://www.w3.org/2001/XMLSchema-instance" xsi:schemaLocation="http://www.sitemaps.org/schemas/sitemap/0.9 http://www.sitemaps.org/schemas/sitemap/0.9/sitemap.xsd">\n')
+
+for x in result:
+    n.write("<url>\n")
+    n.write("<loc>")
+    n.write(moda + x + "</loc>\n</url>\n")
+
+n.write("</urlset>")
+
+
+n.close()
