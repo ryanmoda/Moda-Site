e5aa74e2366d6b4fadce1432b13c7d76fbd5d137
Jack Phelan <jack@modalight.com>
2019-07-23 13:05:42 -0700

    working on job description

--- js/jobs2.js
+++ js/jobs2.js
@@ -2,7 +2,7 @@ function processForm() {
 	var jobList = 
 	{
         "ECRSM": {"Title": "East Coast Regional Sales Manager", "Description": "Lorem ipsum dolor sit amet, consectetuer adipiscing elit, sed diam nonummy nibh euismod tincidunt ut laoreet dolore magna aliquam erat volutpat. Ut wisi enim ad minim veniam, quis nostrud exerci tation"},
-        "NASM": {"Title": "National Accountant Sales Manager", "Description": "Placeholder"},
+        "NASM": {"Title": "National Accounts Sales Manager", "Description": "Placeholder"},
         "VPS": {"Title": "Vice President of Sales", "Description": "Placeholder"},
         "DFT": {"Title": "DMX Field Technician", "Description": "Placeholder"},
         "SE": {"Title": "Software Engineer", "Description": "Placeholder"},
@@ -12,16 +12,18 @@ function processForm() {
         "PRM": {"Title": "Production Manager", "Description": "Placeholder"},
         "AE": {"Title": "Application Engineer", "Description": "Placeholder"},
         "RGC": {"Title": "Regional Sale - Canada", "Description": "Placeholder"},
-        "SVP": {"Title": "Senior Vice President", "Description": "Placeholder"},
+        "SVP": {"Title": "Senior Vice President", "Description": "Placeholder"}
 	}
-	var parameters = location.search.substring(1).split("&");
-	var temp = parameters[0].split("=");
-	l = unescape(temp[1]);
-	console.log(jobList[l]["Description"]);
+        var parameters = location.search.substring(1).split("&");
+        var temp = parameters[0].split("=");
+        l = unescape(temp[1]);
+        
 	var title = jobList[l]["Title"];
 	var description = jobList[l]["Description"];
-
 	document.getElementById("title").innerHTML = title;
-	document.getElementById("description").innerHTML = description;
+        document.getElementById("description").innerHTML = description;
+        
+        console.log("oh boy");
+       window.history.pushState(pushObj, "Jobs", "");
 }
-processForm();
+
