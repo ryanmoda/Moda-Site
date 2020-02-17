815479f41091f97bd97a5deb1364b872896be9f8
Jack Phelan <jack@modalight.com>
2020-02-17 13:47:17 -0800

    attempting to fix isotope

--- js/headerController.js
+++ js/headerController.js
@@ -1,4 +1,6 @@
 (function () { 
+
+var currentPath;
 var app = angular.module('modaApp');
 	var linkFunc = function ($scope) {
 		$scope.picLink = "www.google.com";
@@ -10,8 +12,30 @@ var app = angular.module('modaApp');
   var headerController = function ($scope, $location) 
     { 
         $scope.isActive = function (viewLocation) { 
+			currentPath = $location.path();
+			localStorage.setItem("currentPath", currentPath);
             return viewLocation === $location.path();
-        };
+		};
+		$scope.$on('$locationChangeStart',function(evt, absNewUrl, absOldUrl) {
+
+			var hashIndex = absOldUrl.indexOf('#');
+
+			var oldRoute = absOldUrl.substr(hashIndex + 2);
+
+			History.lastRoute = oldRoute;
+
+			console.log(oldRoute);
+
+			localStorage.setItem("prevPath", "/" + oldRoute);
+
+			if ( localStorage.getItem("prevPath") == "/reps" && localStorage.getItem("currentPath") != "/reps" ) {
+				console.log("yessir");
+
+				localStorage.setItem("visited", "false");
+
+			}
+
+		});
     };
     app.controller('HeaderController',headerController);
 	app.controller('linkFunc', linkFunc);
