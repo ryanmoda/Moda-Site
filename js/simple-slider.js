497a3fec58dd8ae571ae71c0c29c162968dcb1cf
Jack Phelan <jack@modalight.com>
2020-03-20 15:28:00 -0700

    starting nano draft

--- js/simple-slider.js
+++ js/simple-slider.js
@@ -5,18 +5,20 @@
 /* -------------------------- */
 var $scroller = $('.slides');
 // assign click handler
-$('.links a').on('click', function () {  
+$('.links a').on('click', function () { 
+    var docWidth = $(document).width();
+    var factor; 
     $('.links a').each(function() {
         $(this).removeClass("targetedSlide")
         
     });
+    factor = (docWidth > 1050) ? 1000 : 300;
+    
     $(this).addClass("targetedSlide");
     // get the partial id of the div to scroll to
     var divIdx = $(this).attr("id"); 
     divIdx = divIdx.slice(divIdx.length-1) ;
-    divIdx = parseInt(divIdx);
-    console.log(divIdx);
-    
-    divIdx = (divIdx * 1000) - 1000;          
+    divIdx = parseInt(divIdx);    
+    divIdx = (divIdx * factor) - factor;          
     $scroller.scrollLeft(divIdx, 0);
 });
