de7f4e00833b64198e2e423c10946a630b1c8743
Jack Phelan <jack@modalight.com>
2020-03-25 09:56:08 -0700

    starting from scratch

--- js/headerController.js
+++ js/headerController.js
@@ -35,23 +35,23 @@ var app = angular.module('modaApp');
 				localStorage.setItem("visited", "false");
 				
 			}
-			function removejscssfile(filename, filetype){
-				var targetelement=(filetype=="js")? "script" : (filetype=="css")? "link" : "none" //determine element type to create nodelist from
-				var targetattr=(filetype=="js")? "src" : (filetype=="css")? "href" : "none" //determine corresponding attribute to test for
-				var allsuspects=document.getElementsByTagName(targetelement)
-				for (var i=allsuspects.length; i>=0; i--){ //search backwards within nodelist for matching elements to remove
-				 if (allsuspects[i] && allsuspects[i].getAttribute(targetattr)!=null && allsuspects[i].getAttribute(targetattr).indexOf(filename)!=-1) {
-				  allsuspects[i].parentNode.removeChild(allsuspects[i]) //remove element by calling parentNode.removeChild()
-				  console.log("unload + " + allsuspects[i].src);
+			// function removejscssfile(filename, filetype){
+			// 	var targetelement=(filetype=="js")? "script" : (filetype=="css")? "link" : "none" //determine element type to create nodelist from
+			// 	var targetattr=(filetype=="js")? "src" : (filetype=="css")? "href" : "none" //determine corresponding attribute to test for
+			// 	var allsuspects=document.getElementsByTagName(targetelement)
+			// 	for (var i=allsuspects.length; i>=0; i--){ //search backwards within nodelist for matching elements to remove
+			// 	 if (allsuspects[i] && allsuspects[i].getAttribute(targetattr)!=null && allsuspects[i].getAttribute(targetattr).indexOf(filename)!=-1) {
+			// 	  allsuspects[i].parentNode.removeChild(allsuspects[i]) //remove element by calling parentNode.removeChild()
+			// 	  console.log("unload + " + allsuspects[i].src);
 				  
-				 }
-				}
-			   }  
-			   removejscssfile("js/jszip.min.js", "js");
-			   removejscssfile("js/jszip-utils.min.js", "js");
-			   removejscssfile("js/FileSaver.js", "js");
-			   removejscssfile("js/js-help.js", "js");
-			   removejscssfile("js/zip.js", "js");
+			// 	 }
+			// 	}
+			//    }  
+			//    removejscssfile("js/jszip.min.js", "js");
+			//    removejscssfile("js/jszip-utils.min.js", "js");
+			//    removejscssfile("js/FileSaver.js", "js");
+			//    removejscssfile("js/js-help.js", "js");
+			//    removejscssfile("js/zip.js", "js");
 		
 		});
 
