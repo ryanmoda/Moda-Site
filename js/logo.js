b565b7e3b7654f829ed202011aa95a58c3bb90da
Jack Phelan <jack@modalight.com>
2020-03-26 08:12:37 -0700

    Replace logo.js with AngularJS controller approach

--- js/logo.js
+++ js/logo.js
@@ -1,4 +1,5 @@
 //document.getElementById("logo").src="svgs/modalogo.svg";
+
 var logos = document.getElementsByClassName("logo");
 for (var i = 0; i < logos.length; i++) {
 	logos[i].src="svgs/modalogo.svg";
