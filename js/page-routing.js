e8026721d017f69fb9be024da8e84bcbfa78af82
Jack Phelan <jackphelan461@gmail.com>
2020-04-08 14:42:22 -0700

    cache busting randolf's changes

--- js/page-routing.js
+++ js/page-routing.js
@@ -494,7 +494,7 @@
 				templateUrl:'leg-graze-rs.html?1585933729'
 				})
 			.when('/reps',{
-                templateUrl:'reps.html?1586381168'
+                templateUrl:'reps.html?1586382101'
             })
 			.when('/news',{
                 templateUrl:'news.html?1585599141'
