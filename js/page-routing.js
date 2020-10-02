a1703e8f062f54e62313014514f2c767d26d57fa
ryanmoda <info@modalight.com>
2020-10-02 07:33:02 -0700

    Update reps

--- js/page-routing.js
+++ js/page-routing.js
@@ -494,7 +494,7 @@
 				templateUrl:'leg-graze-rs.html?1585933729'
 				})
 			.when('/reps',{
-                templateUrl:'reps.html?1599750000'
+                templateUrl:'reps.html?1601647200'
             })
 			.when('/news',{
                 templateUrl:'news.html?1585599141'
