471dedd5d26d42828b3147f14d652dc056460610
ryanmoda <info@modalight.com>
2020-11-04 09:18:27 -0800

    Revert "Revert "NANO LAUNCH 11.04.2020""
    This reverts commit 917b5684ef6ca109bf6e96454ada66199af814cd.

--- js/select.js
+++ js/select.js
@@ -1,14 +1,16 @@
 $(document).ready(function() {
-	$('.js-cont').select2({placeholder: 'Location', minimumResultsForSearch: -1});
+	$('.js-cont').select2({placeholder: 'Location', minimumResultsForSearch: -1, allowClear: true});
 	$('.js-states').select2({placeholder: 'Select State', allowClear: true});
 	$('.js-provs').select2({placeholder: 'Select Province', allowClear: true});
 	$('.js-test').select2({placeholder: 'Select Location', allowClear: true});
 
 
+
+
+
 //var select2Drop = document.getElementsByClassName('select2-container--default')[1];
 //alert(select2Drop);
 //select2Drop.style.left = "0px !important";
-
 });
 
 
