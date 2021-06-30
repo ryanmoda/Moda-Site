0a1b9660d3cf64be6a7102de1a47143813f4b51a
michaelross <michael@modalight.com>
2021-06-30 13:22:23 -0700

    update

--- js/page-routing.js
+++ js/page-routing.js
@@ -424,7 +424,7 @@
                 templateUrl:'sidebar-grille.html?1623164400'
             	})
               .when('/park-house',{
-                templateUrl:'park-house.html?1625079899'
+                templateUrl:'park-house.html?162508500'
             	})
 				.when('/fremont',{
                 templateUrl:'fremont.html?1623164400'
