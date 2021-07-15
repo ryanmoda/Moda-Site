fc731b0b783cc7587d827baeaf8de91574294d26
michaelross <michael@modalight.com>
2021-07-15 13:59:07 -0700

    update

--- js/page-routing.js
+++ js/page-routing.js
@@ -550,13 +550,10 @@
                 templateUrl:'careers.html?1626363558'
 			})
       .when('/job-description',{
-        templateUrl:'job-description-Project-Manager.html?1626382054'
+                templateUrl:'job-description-Project-Manager.html?1626382054'
       })          
 			.when('/job-description',{
-        templateUrl:'job-description-Quotation-Specialist.html?1626382054'
-      })          
-			.when('/job-description',{
-        templateUrl:'job-description2.html?1626382054'
+                templateUrl:'job-description-Quotation-Specialist.html?1626382054'
       })          
 			.when('/job-description',{
                 templateUrl:'job-description2.html?1626382054'
