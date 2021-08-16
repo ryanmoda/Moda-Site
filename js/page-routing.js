bf65108bd241dc825722014b9ed2ff4758631d23
michaelross <michael@modalight.com>
2021-08-16 08:51:21 -0700

    commit

--- js/page-routing.js
+++ js/page-routing.js
@@ -23,7 +23,7 @@
   app.config(function ($routeProvider){
       $routeProvider
           .when('/',{
-              templateUrl:'homepage.html?1628176232'
+              templateUrl:'homepage.html?1629128982'
 
     })
     // .when('/test',{
