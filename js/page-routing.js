ef52eb7719c871e485030dff6e1d596b66a43f0a
michaelross <michael@modalight.com>
2021-08-25 07:43:10 -0700

    commit

--- js/page-routing.js
+++ js/page-routing.js
@@ -124,13 +124,13 @@
           templateUrl:'graze-dw.html?1629497788',
         })
       .when('/graze-ext',{
-                templateUrl:'graze-ext.html?1629845240'
+                templateUrl:'graze-ext.html?1629845245'
             })
         .when('/graze-wl-ext',{
           templateUrl:'graze-wl-ext.html?1629729572',
         })
         .when('/graze-sc-ext',{
-          templateUrl:'graze-sc-ext.html?1629845240',
+          templateUrl:'graze-sc-ext.html?1629845245',
         })
         .when('/graze-cc-ext',{
           templateUrl:'graze-cc-ext.html?1629497788',
