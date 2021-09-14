f5ec91dac4c76a58ffc7fe826428f1c1824e291a
michaelross <michael@modalight.com>
2021-09-14 13:47:05 -0700

    commit

--- js/page-routing.js
+++ js/page-routing.js
@@ -526,7 +526,7 @@
               templateUrl:'reps.html?1630338860'
           })
     .when('/news',{
-              templateUrl:'news.html?1631649040'
+              templateUrl:'news.html?1631649045'
           })
     .when('/news-2',{
               templateUrl:'news-2.html?1631649036'
