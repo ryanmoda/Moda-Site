b62776ec93aba3d6c3223ae45d8fe5931c16be68
michaelross <michael@modalight.com>
2021-09-27 11:31:21 -0700

    commit

--- js/page-routing.js
+++ js/page-routing.js
@@ -547,10 +547,10 @@
               templateUrl:'careers.html?1632325000'
     })
     .when('/job-description',{
-              templateUrl:'job-description2.html?1632757850'
+              templateUrl:'job-description2.html?1632757855'
     })
     .when('/job-listings',{
-              templateUrl:'job-listings.html?1632757850'
+              templateUrl:'job-listings.html?1632757855'
     })			
     .when('/careers',{
       templateUrl:'careers.html?1632324985'
