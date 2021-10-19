b24f042e9d17d41b9b7ea2a5703045f5e9f53604
michaelross <michael@modalight.com>
2021-10-19 15:32:58 -0700

    commit

--- js/page-routing.js
+++ js/page-routing.js
@@ -292,7 +292,7 @@
               templateUrl:'controls.html?1633727500'
           })
     .when('/converters',{
-              templateUrl:'converters.html?1634681577'
+              templateUrl:'converters.html?1634681585'
           })
     //.when('/dmx-cntrl',{
               //templateUrl:'dmx-cntrl.html?1623164400'
